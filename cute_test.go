3d990adeea565ef962a383b3eea0b33af3030f65
zakaria-chahboun <zakaria.chahboun.1996@gmail.com>
2022-10-12 19:21:56 +0100

    add show case error

--- cute_test.go
+++ cute_test.go
@@ -1,15 +1,26 @@
 package cute
 
-import "testing"
+import (
+	"errors"
+	"testing"
+)
 
 // Test equality of CuteColor type.
 func TestCuteColorType(t *testing.T) {
-	Println("Title", "This is just test", 123456)
-	SetTitleColor(ColorBrightBlue)
-	SetMessageColor(ColorBrightGreen)
-	Println("Hi everyone", "My name is Zakaria!")
 	redColor := CuteColor("\033[31m")
 	if ColorRed != redColor {
 		t.Fail()
 	}
+}
+
+// Test show case.
+func TestCuteFunctions(t *testing.T) {
+	Println("Title", "This is just test", 123456)
+	Printf("Title", "My ID is %v\n", 123456)
+
+	SetTitleColor(ColorBrightBlue)
+	SetMessageColor(ColorBrightGreen)
+	Println("Hi everyone", "My name is Zakaria!")
+
+	Check("Error", errors.New("This is a cute panic!"))
 }
