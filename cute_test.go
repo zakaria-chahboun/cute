aaed9369e86a0db4b39aa036e4034725bb5168de
zakaria-chahboun <zakaria.chahboun.1996@gmail.com>
2022-10-12 18:49:34 +0100

    add print

--- cute_test.go
+++ cute_test.go
@@ -4,6 +4,10 @@ import "testing"
 
 // Test equality of CuteColor type.
 func TestCuteColorType(t *testing.T) {
+	Println("Title", "This is just test", 123456)
+	SetTitleColor(ColorBrightBlue)
+	SetMessageColor(ColorBrightGreen)
+	Println("Hi everyone", "My name is Zakaria!")
 	redColor := CuteColor("\033[31m")
 	if ColorRed != redColor {
 		t.Fail()
