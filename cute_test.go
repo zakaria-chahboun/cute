ac1331979c2d2ec9d970f3d7eb79f6206d34597d
zakaria-chahboun <zakaria.chahboun.1996@gmail.com>
2022-10-17 01:26:10 +0100

    update test

--- cute_test.go
+++ cute_test.go
@@ -15,15 +15,29 @@ func TestCuteColorType(t *testing.T) {
 
 // Test show case.
 func TestCuteFunctions(t *testing.T) {
-	Println("Title", "This is just test", 123456)
-	Printf("Title", "My ID is %v\n", 123456)
+	Println("Println", "Hi! i'm", "Zaki")
+	Printf("Printf", "UID is %v\n", 1399745)
 
 	SetTitleColor(ColorBrightBlue)
 	SetMessageColor(ColorBrightGreen)
-	Printlns("Table", "Row1", "Row2", "Row3")
+	Printlns("Printlns", "Line1", "Line2", "Line3")
 
-	Println("Just head1")
-	Printlns("Just head2")
+	Println("Println: No lines")
+	Printlns("Printlns: No lines")
+
+	list := NewList("Yummy Juice!")
+
+	SetMessageColor(ColorBrightRed)
+	list.Add("4 strawberry")
+
+	SetMessageColor(ColorBrightGreen)
+	list.Add("1 avocado")
+
+	SetMessageColor(ColorWhite)
+	list.Addf("%d ml %s", 500, "milk")
+
+	SetTitleColor(ColorBrightBlue)
+	list.Print()
 
 	Check("Error", errors.New("This is a cute panic!"))
 }
